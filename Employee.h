f2a7d5c93fb80b47c87944722fb93be5bf55e37f
MontserratCL <a01713040@tec.mx>
2025-05-22 17:44:44 -0600

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,1 +1,29 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
 
+#include <string>
+
+class Employee {
+private:
+    std::string name;
+    int id;
+    int age;
+
+public:
+
+// Constructors
+    Employee();
+    Employee(const std::string& name, int id, int age);
+
+    // Getters
+    std::string getName() const;
+    int getId() const;
+    int getAge() const;
+    
+    // Setters
+    void setName(const std::string& name);
+    void setId(int id);
+    void setAge(int age);
+};
+
+#endif 
