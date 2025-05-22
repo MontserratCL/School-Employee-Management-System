73c812cc43598cc85e61a1fbad4bcc9d46eb23a2
MontserratCL <a01713040@tec.mx>
2025-05-22 17:49:46 -0600

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -4,24 +4,24 @@
 Employee:Employee() : name(""), id(0), age(0) {
 }
 // COnstructor 
-Employee:Employee(const std::string& name, int id, int age) 
+Employee:Employee(std::string& name, int id, int age) 
   : name(name), id(id), age(age) {
 }
 // Getters
-std::string Employee::getName() const {
+std::string Employee::getName() {
     return name;
 }
 
-int Employee::getId() const {
+int Employee::getId() {
     return id;
 }
 
-int Employee::getAge() const {
+int Employee::getAge(){
     return age;
 }
 
 // Setters
-void Employee::setName(const std::string& name) {
+void Employee::setName(std::string& name) {
     this->name = name;
 }
 
