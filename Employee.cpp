e7903d6f518fa8cf2f20a28cb9f8d5d111ef7bc0
MontserratCL <a01713040@tec.mx>
2025-05-22 17:47:59 -0600

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -1,1 +1,34 @@
+#include "Employee.h"
 
+//El constructor por defecto 
+Employee:Employee() : name(""), id(0), age(0) {
+}
+// COnstructor 
+Employee:Employee(const std::string& name, int id, int age) 
+  : name(name), id(id), age(age) {
+}
+// Getters
+std::string Employee::getName() const {
+    return name;
+}
+
+int Employee::getId() const {
+    return id;
+}
+
+int Employee::getAge() const {
+    return age;
+}
+
+// Setters
+void Employee::setName(const std::string& name) {
+    this->name = name;
+}
+
+void Employee::setId(int id) {
+    this->id = id;
+}
+
+void Employee::setAge(int age) {
+    this->age = age;
+}
