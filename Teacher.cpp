a986fd6ba267a080e29e1c68f44a5e6455ec9ce3
MontserratCL <a01713040@tec.mx>
2025-05-22 18:00:35 -0600

    Update Teacher.cpp

--- Teacher.cpp
+++ Teacher.cpp
@@ -1,1 +1,41 @@
+#include "Teacher.h"
+#include <sstream>
 
+// Constructor defect
+Teacher::Teacher() : Employee(), subject(""), hours(0) {
+}
+
+// Constructor 
+Teacher::Teacher(std::string name, int id, int age, std::string subject, int hours)
+    : Employee(name, id, age), subject(subject), hours(hours) {
+}
+
+// Getters
+std::string Teacher::getSubject() {
+    return subject;
+}
+
+int Teacher::getHours() {
+    return hours;
+}
+
+// Setters
+void Teacher::setSubject( std::string subject) {
+    this->subject = subject;
+}
+
+void Teacher::setHours(int hours) {
+    this->hours = hours;
+}
+
+// Public Method
+std::string Teacher::getInfo() const {
+    std::stringstream info;
+    info << "Teacher Information:\n";
+    info << "Name: " << getName() << "\n";
+    info << "ID: " << getId() << "\n";
+    info << "Age: " << getAge() << "\n";
+    info << "Subject: " << subject << "\n";
+    info << "Hours: " << hours;
+    return info.str();
+}
